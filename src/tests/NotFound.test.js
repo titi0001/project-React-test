55f26c57b1e394a9c8e0f39725b53de88da8a365
Thiago Pereira <titiura@gmail.com>
2022-08-20 14:47:05 -0300

    Requisito 4- Teste o componente <NotFound.js /> -ok

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente NotFound', () => {
+  it('Teste se a página contém um h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const heading = screen
+      .getByRole('heading', { level: 2, name: /page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+    const img = screen.getByRole('img',
+      { name: /Pikachu crying because the page requested was not found/i });
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
